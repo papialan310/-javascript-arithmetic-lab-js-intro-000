1c6d9d1d45cb884aa316a05be57d097c12a67d8b
papialaan310 <alan.alejandro911@gmail.com>
2016-11-01 17:52:00 +0000

    I'm ready to send my files to github.

---
+++ index.js
@@ -0,0 +1,36 @@
+var add = function(a, b){
+
+return a + b;
+};
+var subtract = function(b, a){
+  return b - a;
+};
+var multiply = function(a, b){
+return a * b;
+};
+var divide = function(a, b){
+
+  return a / b;
+};
+var inc = function(n){
+
+  return n + 1;
+};
+var dec = function(n){
+return n - 1;
+};
+var makeInt = function(n){
+  return parseInt(n, 10);
+};
+
+var assume = function(n){
+return n = 10;
+};
+
+
+var preserveDecimal = function(n){
+  return parseInt(n);
+};
+var preserveDecimal = function(n){
+  return parseFloat(n);
+};
